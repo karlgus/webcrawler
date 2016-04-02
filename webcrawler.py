91e3bd33e400638aec925ace1d309bb9f90056e1
Karl Gustafsson <karlgus@gmail.com>
2016-04-02 04:50:27 +0000

    Added a class Link, that will contain the information about the link. Created a method called openURL that will open the homepage and load it in to a variable.

--- webcrawler.py
+++ webcrawler.py
@@ -5,5 +5,23 @@ import htmldom
 import re
 import time
 
+
+def openURL(url):
+    pass
+
+
+class Link:
+    
+    def __init__(self,url,title):
+        self.url = url
+        self.title = title
+
+    def add_link_to_linkstore(self):
+        pass
+
+    def get_linkstore(self):
+        pass
+
+
 if __name__ == '__main__':
     
