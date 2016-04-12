ab7195b909b68c99feffc8cf31a5c4e539a25d32
Karl Gustafsson <karlgus@gmail.com>
2016-04-12 21:02:10 +0200

    Changed getPage function to only return unique links

--- webcrawler.py
+++ webcrawler.py
@@ -10,7 +10,11 @@ def getPage(url):
     dom = htmldom.HtmlDom(url).createDom()
     #find tag a
     a = dom.find("a")
-    return [link.attr("href") for link in a]
+    link = [link.attr("href") for link in a]
+    unique_link = []
+    [unique_link.append(path) for path in link if path not in unique_link]
+    return unique_link
+    #return [link.attr("href") for link in a]
 
 class Link:
     '''This class will contain information about link object'''
@@ -26,23 +30,32 @@ class Link:
     def get_linkstore(self):
         return self.linkstore
 
-
 if __name__ == '__main__':
 
-
-    links = getPage('http://www.expressen.se')
+    #links = getPage('http://www.expressen.se')
     internal_links = ['/']
-    print(links)
-    link = {0:getPage('http://www.expressen.se' + internal_links[0])}
+    #print(links)
+
+    link = getPage('http://www.expressen.se' + internal_links[0])
+    print(link)
+    #links = []
+    #[links.append(item) for item in link if item not in links]
+    #for path in links:
+    #    if re.match('^/',path):
+    #        print(path)
 
 
+#    for i in range(2):
+#        time.sleep(1)
+#        print('i = {0}'.format(i))
+#        links = {i:getPage('http://aftonbladet.se' + internal_links[i-1][0])}
 #    for i in range(1):
 #        time.sleep(1)
 #        print('i = {0}'.format(i))
 #        links = {i:getPage('http://www.hd.se' + internal_links[i-1][i])}
 #        print(links)
 #        for internal in links:
-#            #print(internal)
+#            print(internal)
 #            #if re.match('(hd.se)',internal) == True or re.match('^/',internal):
 #            if re.match('^/',internal):
 #                internal_links.append(internal)
@@ -64,4 +77,3 @@ if __name__ == '__main__':
     #            print(newurl)
     #        print(counter)
     #        counter += 1
-
